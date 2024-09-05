76f412df59b1bdeb18ec081bae5223c9f294dd42
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-09-05 08:25:45 +0000

    add code to change score

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let currClownTile;
 let currBoyTile;
+let score = 0;
+let gameOver = false;
 
 
 window.onload = function() {
@@ -25,6 +27,9 @@ function getRandomTile() {
 }
 
 function setClown() {
+    if (gameOver) {
+        return;
+    }
 
     if (currClownTile) {
         currClownTile.innerHTML = "";
@@ -35,11 +40,17 @@ function setClown() {
 
     
     let num = getRandomTile();
+    if (currBoyTile && currBoyTile.id == num) {
+        return;
+    }
     currClownTile = document.getElementById(num);
     currClownTile.appendChild(clown);
 }
 
 function setBoy() {
+    if (gameOver) {
+        return;
+    }
 
     if (currBoyTile) {
         currBoyTile.innerHTML = "";
@@ -49,6 +60,9 @@ function setBoy() {
     boy.src = "assets/images/boy.png";
 
     let num = getRandomTile();
+    if (currClownTile && currClownTile.id == num) {
+        return;
+    }
     currBoyTile = document.getElementById(num);
     currBoyTile.appendChild(boy);
 }
@@ -61,10 +75,10 @@ function selectTile() {
     }
     if (this == currClownTile) {
         score += 10;
-        document.getElementById("score").innerText = score.toString(); //update score html
+        document.getElementById("score").innerText = score.toString();
     }
     else if (this == currBoyTile) {
-        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); //update score html
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
         gameOver = true;
     }
 }
